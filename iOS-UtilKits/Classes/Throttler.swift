a285cfb2b42367a0eb5de81026966f8d28fdd9ca
Peter <peter@swag.live>
2020-11-10 15:00:57 +0800

    update throttler follow 3a5180d366b69d46507a74328fb1b75f4438fc1b

--- iOS-UtilKits/Classes/Throttler.swift
+++ iOS-UtilKits/Classes/Throttler.swift
@@ -9,34 +9,29 @@
 import Foundation
 
 @objcMembers
-public class Throttler: NSObject {
-    //https://www.craftappco.com/blog/2018/5/30/simple-throttling-in-swift
+class Throttler: NSObject {
+
     private var workItem: DispatchWorkItem = DispatchWorkItem(block: {})
     private var previousRun: Date = Date.distantPast
     private let queue: DispatchQueue
     private let minimumDelay: TimeInterval
-    
-    public init(minimumDelay: TimeInterval, queue: DispatchQueue = DispatchQueue.main) {
+
+    init(minimumDelay: TimeInterval, queue: DispatchQueue = DispatchQueue.main) {
         self.minimumDelay = minimumDelay
         self.queue = queue
     }
-    
-    public func throttle(_ block: @escaping () -> Void) {
-        // Cancel any existing work item if it has not yet executed
+
+    func throttle(_ block: @escaping () -> Void) {
         workItem.cancel()
-        
-        // Re-assign workItem with the new block task, resetting the previousRun time when it executes
         workItem = DispatchWorkItem() {
             [weak self] in
             self?.previousRun = Date()
             block()
         }
-        
-        // If the time since the previous run is more than the required minimum delay
-        // => execute the workItem immediately
-        // else
-        // => delay the workItem execution by the minimum delay time
-        let delay = previousRun.timeIntervalSinceNow > minimumDelay ? 0 : minimumDelay
+
+        let period = abs(previousRun.timeIntervalSinceNow)
+        let delay = period > minimumDelay ? 0 : minimumDelay
+
         queue.asyncAfter(deadline: .now() + Double(delay), execute: workItem)
     }
 }
