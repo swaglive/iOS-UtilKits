cc869491b84254c03b57f83b0142253261ea21a7
finn <finn.he@swag.live>
2021-04-06 14:15:50 +0800

    move file add set public

--- iOS-UtilKits/Classes/Data+Extension.swift
+++ iOS-UtilKits/Classes/Data+Extension.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension Data {
-    var prettyPrintedJSONString: String? {
+    public var prettyPrintedJSONString: String? {
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
               let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
               let prettyPrintedString = String(data: data, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue)) else { return nil }
